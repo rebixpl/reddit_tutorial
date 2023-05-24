7972995a239cd256f7cdad2d7b7d12027c84280d
rebixpl <33410494+rebixpl@users.noreply.github.com>
2023-05-25 01:44:16 +0200

    Storing Data to Provider

--- lib/features/auth/controller/auth_controller.dart
+++ lib/features/auth/controller/auth_controller.dart
@@ -2,22 +2,41 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:reddit_tutorial/core/utils.dart';
 import 'package:reddit_tutorial/features/auth/repository/auth_repository.dart';
+import 'package:reddit_tutorial/models/user_model.dart';
 
-final authControllerProvider = Provider<AuthController>(
+final userProvider = StateProvider<UserModel?>((ref) => null);
+
+final authControllerProvider = StateNotifierProvider<AuthController, bool>(
   (ref) => AuthController(
-    authRepository: ref.read(authRepositoryProvider),
+    authRepository: ref.watch(authRepositoryProvider),
+    ref: ref,
   ),
 );
 
-class AuthController {
+class AuthController extends StateNotifier<bool> {
   final AuthRepository _authRepository;
+  final Ref _ref;
 
-  AuthController({required AuthRepository authRepository})
-      : _authRepository = authRepository;
+  AuthController({
+    required AuthRepository authRepository,
+    required Ref ref,
+  })  : _authRepository = authRepository,
+        _ref = ref,
+        super(false); // loading is false by default
 
   void signInWithGoogle(BuildContext context) async {
+    state = true; // loading is true when we start the request
     final user = await _authRepository.signInWithGoogle();
-    // Left is an error, Right is a success
-    user.fold((l) => showSnackBar(context, l.message), (r) => null);
+    state = false; // loading is false when we finish the request
+    // Left is an error, Right is a success (userModel)
+    user.fold(
+      (l) => showSnackBar(
+        context,
+        l.message,
+      ),
+      (userModel) => _ref.read(userProvider.notifier).update(
+            (state) => userModel,
+          ),
+    );
   }
 }
--- lib/features/auth/screen/login_screen.dart
+++ lib/features/auth/screen/login_screen.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:reddit_tutorial/core/common/loader.dart';
 import 'package:reddit_tutorial/core/common/sign_in_button.dart';
 import 'package:reddit_tutorial/core/constants/constants.dart';
+import 'package:reddit_tutorial/features/auth/controller/auth_controller.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends ConsumerWidget {
   const LoginScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isLoading = ref.watch(authControllerProvider);
     return Scaffold(
       appBar: AppBar(
         title: Image.asset(
@@ -25,29 +29,31 @@ class LoginScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Column(
-        children: [
-          const SizedBox(height: 30.0),
-          const Text(
-            'Dive into anything!',
-            style: TextStyle(
-              fontSize: 24.0,
-              letterSpacing: 0.5,
-              fontWeight: FontWeight.bold,
+      body: isLoading
+          ? const Loader()
+          : Column(
+              children: [
+                const SizedBox(height: 30.0),
+                const Text(
+                  'Dive into anything!',
+                  style: TextStyle(
+                    fontSize: 24.0,
+                    letterSpacing: 0.5,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 30.0),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Image.asset(
+                    Constants.loginEmotePath,
+                    height: 400.0,
+                  ),
+                ),
+                const SizedBox(height: 20.0),
+                const SignInButton(),
+              ],
             ),
-          ),
-          const SizedBox(height: 30.0),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Image.asset(
-              Constants.loginEmotePath,
-              height: 400.0,
-            ),
-          ),
-          const SizedBox(height: 20.0),
-          const SignInButton(),
-        ],
-      ),
     );
   }
 }
