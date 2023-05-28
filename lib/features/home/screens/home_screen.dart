bed370dcc1625b865564442fc40e83f146462bc0
rebixpl <33410494+rebixpl@users.noreply.github.com>
2023-05-28 10:26:45 +0200

    Community List Flutter Drawer

--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:reddit_tutorial/features/auth/controller/auth_controller.dart';
+import 'package:reddit_tutorial/features/home/drawers/community_list_drawer.dart';
 
 class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
+
+  void displayDrawer(BuildContext context) {
+    Scaffold.of(context).openDrawer();
+  }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -13,9 +18,14 @@ class HomeScreen extends ConsumerWidget {
       appBar: AppBar(
         title: const Text('Home'),
         centerTitle: false,
-        leading: IconButton(
-          icon: const Icon(Icons.menu),
-          onPressed: () {},
+        leading: Builder(
+          // we use builder here to get the context, so we can open the drawer
+          builder: (context) {
+            return IconButton(
+              icon: const Icon(Icons.menu),
+              onPressed: () => displayDrawer(context),
+            );
+          },
         ),
         actions: [
           IconButton(
@@ -34,6 +44,7 @@ class HomeScreen extends ConsumerWidget {
       body: Center(
         child: Text(user.name),
       ),
+      drawer: const CommunityListDrawer(),
     );
   }
 }
--- lib/router.dart
+++ lib/router.dart
@@ -1,16 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:reddit_tutorial/features/auth/screen/login_screen.dart';
+import 'package:reddit_tutorial/features/community/screens/create_community_screen.dart';
 import 'package:reddit_tutorial/features/home/screens/home_screen.dart';
 import 'package:routemaster/routemaster.dart';
 
 final loggedOutRoute = RouteMap(
   routes: {
-    '/': (_) => const MaterialPage(child: LoginScreen()),
+    '/': (_) => const MaterialPage(
+          child: LoginScreen(),
+        ),
   },
 );
 
 final loggedInRoute = RouteMap(
   routes: {
-    '/': (_) => const MaterialPage(child: HomeScreen()),
+    '/': (_) => const MaterialPage(
+          child: HomeScreen(),
+        ),
+    '/create-community': (_) => const MaterialPage(
+          child: CreateCommunityScreen(),
+        ),
   },
 );
